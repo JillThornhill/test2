a1c1fe6f8d27a84176b5f89dbd279fc262c4c3dd
Jill <jill.thornhill@zol.co.zw>
2023-02-14 14:03:55 +0200

    Swapped repos

--- flake.nix
+++ flake.nix
@@ -7,11 +7,14 @@
     nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
+      environment.systemPackages = [ nixpkgs.hello ];
       modules = [
-           ./universal_dhcp.nix
+        ({modulesPath, ... }: {
+          imports = [
+            (modulesPath + "/installer/scan/not-detected.nix")
+            (modulesPath + "/profiles/qemu-guest.nix")
             disko.nixosModules.disko
-          
-          {
+          ];
           disko.devices = import ./disk-config.nix {
             lib = nixpkgs.lib;
           };
@@ -20,7 +23,10 @@
             efiSupport = true;
             efiInstallAsRemovable = true;
           };
-        }
+          services.openssh.enable = true;
+           users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICZ5JOx5EYZuX0sV6u0vWCkxfX03sbHl148bry1NwNuW root@ubuntu-4gb-hel1-2" ];
+ #         users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKREKbihvckSohdmjQFZELhNNhSHdaoO9zPUUUCll9Y0 jill@Ubuntu" ];
+        })
       ];
     };
   };
